da78206d7bf7e5e44dbefd91d31683b4f3185b92
Fred Nielsen <16697069+fredless@users.noreply.github.com>
2019-10-16 07:18:55 -0700

    reod w/ stateful alerting and periodic monitoring

--- Remote Monitoring Alert/README.md
+++ Remote Monitoring Alert/README.md
@@ -4,7 +4,7 @@
 
 *contributed by [Fred Nielsen](https://github.com/fredless) @ [ePlus Technology](https://www.eplus.com)*
 
-Provides on-screen alerts to OSD and Touch display when remote monitoring via the admin UI is taking place.  Script refires alert whenever a `VideoSnapshotTaken` event is seen, and when continuous refresh is toggled in the UI this event refires periodically.  Duration may need to be adjusted based on codec model and desired behavior.
+Provides on-screen alerts to OSD and Touch display when remote monitoring via the admin UI is taking place.  Script fires alert whenever a new `VideoSnapshotTaken` event is seen, and watches for further events typically seen when continuous refresh is toggled in the UI.  Interval constant may need to be adjusted based on codec model and desired behavior.
 
 Not extensively tested across a broad selection of code or systems, further contributions and issues welcome.
 
--- Remote Monitoring Alert/remoteMonitoringAlert.js
+++ Remote Monitoring Alert/remoteMonitoringAlert.js
@@ -1,17 +1,38 @@
 const xapi = require('xapi');
 
-// Tune message duration (in seconds) and notice to match your deployment needs
-const DURATION = 6;
+// Tune notice to match your deployment needs
 const MESSAGE = "System is being remotely monitored by an administrator";
 
-function remoteMonitorAlert() {
-    console.log('Remote monitoring detected, displaying alert');
-    // Displays Alert on both Touch and OSD
-    xapi.command('UserInterface Message Alert Display', {Duration: DURATION, Text:MESSAGE})
-    
-    // Displays Text on OSD only
-    // xapi.command('UserInterface Message TextLine Display', {x:1, y:1,
-    //                                                        Duration: DURATION, Text:MESSAGE})
+// To prevent realerts, this interval needs to be longer by some seconds than trigger fires when
+// continuous snapshot refresh is enabled.  May vary by codec, browser and software.
+// Also impacts how long message remains up after monitoring is stopped.
+const CHECK_INTERVAL = 15;
+
+// // Gloabl state flags
+var alertOn = false
+var snapshotTrigger = false
+
+function remoteMonitorTriggered() {
+  // Log and store snapshot event state
+  console.log('Snapshot event triggered');
+  snapshotTrigger = true;
+  // Only set Alert if not already set
+  if(!alertOn){
+    xapi.command('UserInterface Message Alert Display', {Text:MESSAGE});
+    alertOn = true;
+  }
 }
 
-xapi.event.on('VideoSnapshotTaken', remoteMonitorAlert);
+setInterval(function(){
+  // If snapshots have discontinued since last interval, clear alert
+  if(!snapshotTrigger && alertOn){
+    xapi.command('UserInterface Message Alert Clear');
+    alertOn = false;
+  }
+  // Reset state until next check, continuous monitoring will retrip flag when enabled
+  else if(snapshotTrigger){
+    snapshotTrigger = false;
+  }
+}, CHECK_INTERVAL * 1000);
+  
+xapi.event.on('VideoSnapshotTaken', remoteMonitorTriggered);
