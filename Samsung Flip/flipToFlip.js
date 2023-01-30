5ebec0984b525bf596afe58db34f75eea9c33580
Tore Bjolseth <tbjolset@cisco.com>
2023-01-30 10:25:30 +0100

    Samsung flip: set expected touch panels to 0
    to avoid diagnostic errors in control hub (?)

--- Samsung Flip/flipToFlip.js
+++ Samsung Flip/flipToFlip.js
@@ -1,5 +1,14 @@
 import xapi from 'xapi';
 
+
+// Don't want error diagnostics for this:
+try {
+  xapi.Config.Peripherals.Profile.TouchPanels.set(0);
+}
+catch(e) {
+  console.warn('Not able to set touch panel ')
+}
+
 function onPanelClicked(event) {
   if (event.PanelId === 'webexOnFlip') {
     xapi.Command.Video.CEC.Output.SendInactiveSourceRequest();
